82fbdfaf55fa737b6c3b8b552cea0cf2861d0792
Amir Ali V <amiralivillan@gmail.com>
2025-06-19 16:09:53 +0530

    updated some ui

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,20 +1,58 @@
 import React from "react";
 import personalImage from "../assets/personal-image.jpg";
-import ShinyText from "../library/ShinyText";
+import { ShineBorder } from "../library/ShineBorder";
+import { cn } from "../lib/utils";
+import { AnimatedShinyText } from "../library/AnimatedShinyText";
+import { ArrowRightIcon } from "lucide-react";
+import { CoolMode } from "../library/CoolMode";
+import { TextShimmer } from "../library/TextShimmer";
 
 export const Home = () => {
   return (
     <div>
       <div className="h-screen overflow-hidden relative">
-        <div className="absolute inset-0 z-20 flex items-center justify-center">
-          
+        <div className="absolute inset-0 z-20 flex flex-col gap-8 items-center justify-center">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <div
+              className={cn(
+                "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-100 dark:border-white/15 dark:bg-transparent dark:hover:bg-neutral-800"
+              )}
+            >
+              <CoolMode>
+                <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out text-sm hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+                  <span>✨ I help brands turn bold ideas !</span>
+                  <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+                </AnimatedShinyText>
+              </CoolMode>
+            </div>
+            <TextShimmer className="font-mono text-[15px]" duration={2}>
+              into beautiful & functional digital products.
+            </TextShimmer>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h1 className="text-7xl font-bold text-neutral-400/50 font-poiret tracking-wide">
+              Hi, I’m <span className="text-neutral-400">Amir Ali V</span>
+            </h1>
+            <p className="text-lg font-mono text-neutral-400/50 tracking-wide">
+              Full Stack Developer | UI/UX Enthusiast
+            </p>
+          </div>
         </div>
+
+        <div className="absolute -right-2 top-0 z-20 flex w-[200px] rounded-l-full h-full ">
+          <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
+        </div>
+
+        <div className="absolute -left-2 top-0 z-20 flex w-[200px]  rounded-r-full h-full ">
+          <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
+        </div>
+
         <div className="absolute inset-0 z-0 h-full w-full">
           <img
             src={personalImage}
             alt="Personal"
             className="object-cover object-top justify-center w-full h-full"
-            style={{ objectPosition: 'center center' }}
+            style={{ objectPosition: "center center" }}
           />
         </div>
         <div className="absolute bottom-0 left-0 right-0 z-10 flex items-center justify-center h-28 bg-gradient-to-b from-transparent to-black"></div>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss({
+      theme: {
+        extend: {
+          keyframes: {
+            shine1: {
+              "0%": { "background-position": "100%" },
+              "100%": { "background-position": "-100%" },
+            },
+          },
+          animation: {
+            shine1: "shine 5s linear infinite",
+          },
+        },
+      },
+      plugins: [],
+    }),
   ],
-})
+});
